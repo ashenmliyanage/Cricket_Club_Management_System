325453372596494e28acca37abe3b28ed6d191fa
AshenMadhushanka <glamadushanka2@gmail.com>
2024-01-05 12:32:36 +0530

    OTP form creation

---
+++ src/main/java/lk/ijse/Controller/OTPFormController.java
@@ -0,0 +1,25 @@
+package lk.ijse.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class OTPFormController {
+    @FXML
+    private TextField fild;
+
+    @FXML
+    void backbtnOnActhion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reSendOnActhion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verifyOnActhion(ActionEvent event) {
+
+    }
+}
