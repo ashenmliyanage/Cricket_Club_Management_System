f119b39779356f914432a7bb93903c16e2436474
AshenMadhushanka <glamadushanka2@gmail.com>
2024-01-09 18:26:14 +0530

    CreateAccountBoImpl

--- src/main/java/lk/ijse/BO/custom/impl/CreateAccountBoImpl.java
+++ src/main/java/lk/ijse/BO/custom/impl/CreateAccountBoImpl.java
@@ -3,13 +3,14 @@ package lk.ijse.BO.custom.impl;
 import lk.ijse.BO.custom.CreateAccountBo;
 import lk.ijse.Dao.Custom.UserDao;
 import lk.ijse.Dao.Custom.impl.UserDaoImpl;
+import lk.ijse.Dao.DAOFactory;
 import lk.ijse.Model.UserDto;
 import lk.ijse.entity.User;
 
 import java.sql.SQLException;
 
 public class CreateAccountBoImpl implements CreateAccountBo {
-    UserDao userDao = new UserDaoImpl();
+    UserDao userDao = (UserDao) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOType.User);
 
     @Override
     public String generateId(String colum, String table, String type) throws SQLException, ClassNotFoundException {
